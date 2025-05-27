04303dee50b0b2b88a435565583867653e5450b7
ArsKostromin <arsyakost@gmail.com>
2025-05-27 21:16:08 +0500

    q

--- src/keyboards.py
+++ src/keyboards.py
@@ -3,7 +3,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 def star_amount_keyboard():
     return InlineKeyboardMarkup(inline_keyboard=[
         [
-            InlineKeyboardButton(text="⭐ 5", callback_data="pay:1"),
+            InlineKeyboardButton(text="⭐ 1", callback_data="pay:1"),
             InlineKeyboardButton(text="⭐ 15", callback_data="pay:15"),
             InlineKeyboardButton(text="⭐ 27", callback_data="pay:27"),
             InlineKeyboardButton(text="⭐ 48", callback_data="pay:48"),
