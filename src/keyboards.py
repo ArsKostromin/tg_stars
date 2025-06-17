5ead6476cce68a8c68a105531eae5921c435ea9d
SKYFALL <arsyakost@gmail.com>
2025-06-17 16:03:07 +0500

    price

--- src/keyboards.py
+++ src/keyboards.py
@@ -3,9 +3,9 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 def star_amount_keyboard():
     return InlineKeyboardMarkup(inline_keyboard=[
         [
-            InlineKeyboardButton(text="⭐ 77~1$", callback_data="pay:77"),
-            InlineKeyboardButton(text="⭐ 1160~15", callback_data="pay:1160"),
-            InlineKeyboardButton(text="⭐ 2080~27$", callback_data="pay:2080"),
+            InlineKeyboardButton(text="⭐ 550~7$", callback_data="pay:550"),
+            InlineKeyboardButton(text="⭐ 1200~15", callback_data="pay:1200"),
+            InlineKeyboardButton(text="⭐ 2100~27$", callback_data="pay:2100"),
             InlineKeyboardButton(text="⭐ 3700~48$", callback_data="pay:3700"),
         ],
         [
