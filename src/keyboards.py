7ce97f2198482a11ba32713c74c77f8e25f50731
SKYFALL <arsyakost@gmail.com>
2025-06-19 12:41:05 +0500

    q

--- src/keyboards.py
+++ src/keyboards.py
@@ -4,7 +4,7 @@ def star_amount_keyboard():
     return InlineKeyboardMarkup(inline_keyboard=[
         [
             InlineKeyboardButton(text="⭐ 550~7$", callback_data="pay:550"),
-            InlineKeyboardButton(text="⭐ 1200~15", callback_data="pay:1200"),
+            InlineKeyboardButton(text="⭐ 1200~15$", callback_data="pay:1200"),
             InlineKeyboardButton(text="⭐ 2100~27$", callback_data="pay:2100"),
             InlineKeyboardButton(text="⭐ 3700~48$", callback_data="pay:3700"),
         ],
